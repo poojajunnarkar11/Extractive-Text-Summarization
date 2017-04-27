8668c0296f3406f1e68528647857e9ef3350bb5a
Pooja Junnarkar <pooja.junnarkar@copart.com>
2017-04-26 20:11:46 -0500

    modified file names

--- textrank.py
+++ textrank.py
@@ -1,43 +1,11 @@
-"""
-TextRank algorithm: https://web.eecs.umich.edu/~mihalcea/papers/mihalcea.emnlp04.pdf
-
-Dependencies: nltk, numpy, networkx
-
-"""
-
 import nltk
 import itertools
-from operator import itemgetter
 import networkx as nx
 import os
 import io
 
-#apply syntactic filters based on POS tags
-def filter_for_tags(tagged, tags=['NN', 'JJ', 'NNP']):
-    return [item for item in tagged if item[1] in tags]
-
-def normalize(tagged):
-    return [(item[0].replace('.', ''), item[1]) for item in tagged]
-
-def unique_everseen(iterable, key=None):
-    "List unique elements, preserving order. Remember all elements ever seen."
-    # unique_everseen('AAAABBBCCDAABBB') --> A B C D
-    # unique_everseen('ABBCcAD', str.lower) --> A B C D
-    seen = set()
-    seen_add = seen.add
-    if key is None:
-        for element in itertools.ifilterfalse(seen.__contains__, iterable):
-            seen_add(element)
-            yield element
-    else:
-        for element in iterable:
-            k = key(element)
-            if k not in seen:
-                seen_add(k)
-                yield element
-
-def lDistance(firstString, secondString):
-    "Function to find the Levenshtein distance between two words/sentences - gotten from http://rosettacode.org/wiki/Levenshtein_distance#Python"
+def levenshtein_dist(firstString, secondString):
+    "Function to find the Levenshtein distance between two sentences - gotten from http://rosettacode.org/wiki/Levenshtein_distance#Python"
     if len(firstString) > len(secondString):
         firstString, secondString = secondString, firstString
     distances = range(len(firstString) + 1)
@@ -61,18 +29,17 @@ def buildGraph(nodes):
     for pair in nodePairs:
         firstString = pair[0]
         secondString = pair[1]
-        levDistance = lDistance(firstString, secondString)
+        levDistance = levenshtein_dist(firstString, secondString)
         gr.add_edge(firstString, secondString, weight=levDistance)
 
     return gr
 
-def extractSummary(text):
+def extract_summary(text):
     sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
     sentenceTokens = sent_detector.tokenize(text.strip())
     graph = buildGraph(sentenceTokens)
 
     calculated_page_rank = nx.pagerank(graph, weight='weight')
-
     #most important sentences in ascending order of importance
     sentences = sorted(calculated_page_rank, key=calculated_page_rank.get, reverse=True)
 
@@ -84,24 +51,23 @@ def extractSummary(text):
 
     return summary
 
-def writeFiles(summary, fileName):
-    print "Generating summaries to " + 'summaries/' + fileName
+def write_files(summary, fileName):
+    print '\nGenerating summary to ' + 'summaries/' + fileName
     summaryFile = io.open('summaries/' + fileName, 'w')
     summaryFile.write(summary)
     summaryFile.close()
 
-    print "\n-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"
+    print '\n-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-'
 
-# Get all .txt files from /articles
-articles = []
-for textfiles in os.listdir("articles"):
-    if textfiles.endswith(".txt"):
-        articles.append(textfiles)
-print articles
-for article in articles:
-    print 'Summarizing article from articles/' + article
-    articleFile = io.open('articles/' + article, 'r')
-    text = articleFile.read()
-    summary = extractSummary(text)
-    print summary
-    writeFiles(summary, article)
+# Get all .txt files from /documents
+documents = []
+for text_files in os.listdir("documents"):
+    if text_files.endswith(".txt"):
+        documents.append(text_files)
+for text_file in documents:
+    print '\nSummarizing text from documents/' + text_file
+    doc_file = io.open('documents/' + text_file, 'r')
+    text = doc_file.read()
+    summary = extract_summary(text)
+    print '\nSummary for document: '+ text_file + "\n" + summary
+    write_files(summary, text_file)
